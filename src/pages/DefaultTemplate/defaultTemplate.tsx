49b0721a65bc81b040ddd35d2c998a7101c7c5db
Alana Maia <alanaalvesmaia@gmail.com>
2024-02-19 15:15:04 -0300

    refactor: video (wip)

--- src/pages/DefaultTemplate/defaultTemplate.tsx
+++ src/pages/DefaultTemplate/defaultTemplate.tsx
@@ -1,6 +1,5 @@
 import { Footer } from "../../components/Footer/footer";
 import { Header } from "../../components/Header/header";
-import styles from "./style.module.scss";
 import { ChildrenProps } from "../../interfaces/interfaces";
 
 export const DefaultTemplate = ({ children }: ChildrenProps) => {
